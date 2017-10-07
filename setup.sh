37bac9d71ca0b6925f2932aa03ea0e44924710a0
Rostislav Beneš <r.dee.b.b@gmail.com>
2017-10-07 11:03:22 +0200

    no silent

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 #curl https://nixos.org/nix/install | sh
-travis_wait 30 nix-build -Q -A go  --cores 8
+nix-build -A go  --cores 8
 nix-env -iA nixpkgs.tree
 nix-build -A go.x.text
 #tree ./result-bin
